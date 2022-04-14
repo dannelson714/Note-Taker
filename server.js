8ee427e35913ab741eeb5b3ea9eca33d9c506903
Daniel Nelson <dan.s.nelson@gmail.com>
2022-04-13 20:51:41 -0400

    Added file reading/writing to connect establish persistent data

--- server.js
+++ server.js
@@ -29,6 +29,44 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
+app.post('/api/notes', (req,res) => {
+    console.info(`${req.method} request received to add a new note`);
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+        
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                parsedNotes.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) => writeErr ? console.error(writeErr) : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        }
+
+        console.log(response);
+        res.status(201).json(response);
+    }
+    else {
+        res.status(500).json('Error in posting review');
+    }
+
+
+
+})
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
